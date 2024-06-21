22fdbfe9c59b6a19407be564f0d529726654085b
Tony Froget <tonyfrogetdev@gmail.com>
2024-06-21 19:36:53 +0200

    fix

--- data/data.tsx
+++ data/data.tsx
@@ -36,25 +36,25 @@ export const popupItems = [
     title: "HTML - CSS - JavaScript",
     description: "Mes compétences en HTML et CSS me permettent de créer des designs modernes et épurés, alliant esthétisme et fonctionnalité pour une expérience utilisateur optimale.",
     link: "https://tonyfrogetdev.github.io/numeralternance/",
-    image: "/NumerCapture.png",
+    image: "/NumerCapture.PNG",
   },
   {
     title: "ReactJs - TailwindCSS",
     description: "Mes compétences en React & TailwindCSS me permettent d'optimiser la création de parties front-end rapidement pour des applications plus complexes, alliant esthétisme et fonctionnalité pour une expérience utilisateur optimale.",
     link: "https://github.com/tonyfrogetdev/TsaraJetLuxe",
-    image: "/TsaraJetLuxe.png",
+    image: "/TsaraJetLuxe.PNG",
   },
   {
     title: "NextJs - TailwindCss",
     description: "EN CONSTRUCTION - Mes compétences en NextJs me permettent de créer une application de HomeService pour Madagascar, où VitaTsara pourra mettre en relation les clients et prestataires facilement comme wecasa mais avec les réalités de Madagascar",
     link: "https://www.madagascode.com/",
-    image: "/VitaTsara.png",
+    image: "/VitaTsara.PNG",
   },
   {
     title: "Créations de contenu",
     description: "Ma passion pour la création de contenu se manifeste à travers l'utilisation de plateformes comme Canva et YouTube. Grâce à Canva, je réalise des designs visuellement attrayants pour diverses applications, allant des publications sur les réseaux sociaux aux présentations professionnelles. En parallèle, ma chaîne YouTube me permet de partager mon quotidien dans l'apprentissage du développement Web, j'ai bien progressé depuis mon premier challenge",
     link: "https://www.youtube.com/watch?v=CsOyeztonG8",
-    image: "/Contenu.png",
+    image: "/Contenu.PNG",
   },
 ];
 
